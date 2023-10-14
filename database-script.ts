588659348ee2418b68486a681d41af172ccb2ec3
KaykyDeSouzaDias <kaykydias871@gmail.com>
2023-10-14 15:17:13 -0300

    Feat: update gist functionality & youtube api fetching fix

--- database-script.ts
+++ database-script.ts
@@ -1,5 +1,5 @@
-import axios from "axios";
-import { ILivestreams, ILivestreamsItems } from "./livestreams";
+import { ILivestreams } from "./livestreams";
+import { Octokit } from "octokit";
 
 const CHANNEL_IDS = [
   "UC2fVSthyWxWSjsiEAHPzriQ",
@@ -25,56 +25,56 @@ const CHANNEL_IDS = [
   "UCPPGd9KtOnMKRoIQOHWAbow",
 ];
 
-const gistId = "35c088071883c9e93952bd6fbd5ad5ff";
-const gistApiUrl = `https://api.github.com/gists/${gistId}`;
+const GIST_ID = "35c088071883c9e93952bd6fbd5ad5ff";
+const gistApiUrl = `PATCH /gists/${GIST_ID}`;
 
 const GITHUB_TOKEN = process.env.DATABASE_GIST_PERSONAL_TOKEN;
 const YOUTUBE_API_KEY = process.env.YOUTUBE_API_KEY;
 
 async function getYouTubeLiveInfo(channelId: string) {
   try {
-    const response = (await axios
-      .get(
-        `https://www.googleapis.com/youtube/v3/search?key=${YOUTUBE_API_KEY}&part=snippet,id&channelId=${channelId}&maxResults=50&q=live`
-      )
-      .then((d) => d.data.json())) as ILivestreams;
+    const response = (await fetch(
+      `https://www.googleapis.com/youtube/v3/search?key=${YOUTUBE_API_KEY}&part=snippet,id&channelId=${channelId}&maxResults=50&q=live`
+    ).then((data) => data.json())) as ILivestreams;
 
-    return response.items.filter(
+    return response.items?.filter(
       (i) => i.snippet.liveBroadcastContent === "live" && i.id.videoId
     );
   } catch (error) {
-    console.error("Erro ao buscar informações das lives do YouTube:", error);
+    console.error(
+      "Error during the search for Youtube API live streams:",
+      error
+    );
     return [];
   }
 }
 
+const octokit = new Octokit({
+  auth: GITHUB_TOKEN,
+});
+
 async function updateGitHubGist(content: string) {
   try {
-    const response = await axios.patch(
-      gistApiUrl,
-      {
-        files: {
-          "mood-fi_livestream_database.json": {
-            content,
-          },
+    const response = await octokit.request(gistApiUrl, {
+      gist_id: GIST_ID,
+      files: {
+        "mood-fi_livestream_database.json": {
+          content: content,
         },
       },
-      {
-        headers: {
-          Authorization: `token ${GITHUB_TOKEN}`,
-        },
-      }
-    );
+      headers: {
+        "X-GitHub-Api-Version": "2022-11-28",
+      },
+    });
 
-    console.log("Gist atualizado com sucesso:", response.data);
+    console.log("Gist update success:", response.data);
   } catch (error) {
-    console.error("Erro ao atualizar o Gist do GitHub:", error);
+    console.error("Gist update error:", error);
   }
 }
 
-for (const channel of CHANNEL_IDS) {
-  getYouTubeLiveInfo(channel).then((liveInfo) => {
-    const formattedInfo = JSON.stringify(liveInfo, null, 2);
-    updateGitHubGist(formattedInfo);
-  });
-}
+const promises = CHANNEL_IDS.map((channelId) => getYouTubeLiveInfo(channelId));
+
+Promise.all(promises).then((results) => {
+  updateGitHubGist(JSON.stringify(results.flat()));
+});
